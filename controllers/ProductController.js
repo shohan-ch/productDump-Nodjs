e9d809b0a7367bf255484a82569e41f0519b4df4
shohan-ch <shohan889@yahoo.com>
2023-03-25 02:53:26 +0600

    Fix:product id get form query parameter and single product fetch

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,28 @@
-const  Product  = require("../models/Product");
+const Product = require("../models/Product");
 
-exports.getAll = async (res) =>{
-    try {
-        const product = await Product.find({});
-        res.end(JSON.stringify(product));
-    } catch (error) {
-        console.log(error.message)
+exports.getAll = async (res) => {
+  try {
+    const product = await Product.find({});
+    res.end(JSON.stringify(product));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+exports.getOne = async (res, id) => {
+  try {
+    const singleProduct = await Product.findOne({
+      _id: id,
+    });
+    if (singleProduct) {
+      res.end(JSON.stringify(singleProduct));
+    } else {
+      res.writeHead(404, {
+        "Content-Type": "text/plain",
+      });
+      res.end("Product not found!");
     }
-
-}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,12 @@
 const productController = require("../controllers/ProductController");
 exports.route = (req, res) => {
   const { url } = req;
-  console.log(req.headers);
-  const { pathname, searchParams } = new URL(url, `http://${req.headers.host}`);
-  if (pathname === "/") productController.getAll(res);
+  const urlObj = new URL(url, `http://${req.headers.host}`);
+  // console.log(urlObj);
+  const { href, searchParams, origin } = urlObj;
+  const productRoute = `${origin}/products`;
+  const id = searchParams.get("id");
+  if (href === productRoute) productController.getAll(res);
+  if (href === productRoute + `?id=${id}` && req.method == "GET")
+    productController.getOne(res, id);
 };
