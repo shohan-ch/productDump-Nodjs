21662178d331e803e365bdeca252442f3c1dfa43
shohan-ch <shohan889@yahoo.com>
2023-03-24 23:45:06 +0600

    urlObject created

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,7 @@
-const productController =  require('../controllers/ProductController')
-exports.route  = (req, res) =>{
-    const {url} = req;
-    if(url==="/") productController.getAll(res)
-
-}
+const productController = require("../controllers/ProductController");
+exports.route = (req, res) => {
+  const { url } = req;
+  console.log(req.headers);
+  const { pathname, searchParams } = new URL(url, `http://${req.headers.host}`);
+  if (pathname === "/") productController.getAll(res);
+};
